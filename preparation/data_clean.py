411976735d0a92f6359cb5414fd357c932179cc3
Your-Full-Name <alfonso.ridolfo@gmail.com>
2021-10-10 01:08:04 +0200

    chore: initial commit

---
+++ preparation/data_clean.py
@@ -0,0 +1,33 @@
+def clean_data(df, target_feature):
+    """
+    This function cleans df using the following steps to produce X and y:
+    1. Drop all the rows with no preferred language
+    2. Create X as all the columns that are not the Salary column
+    3. Create y as the Salary column
+    4. For each numeric variable, fill the column with the mean value.
+    6. Create dummy columns for all the categorical variables, drop the original columns
+    :param df dataframe to be cleaned
+    :returns X - A matrix holding all of the variables you want to consider when predicting the response
+             y - the corresponding response vector
+    """
+    # Drop rows with missing target_feature values
+    df = df.dropna(subset=[target_feature], axis=0)
+    y = df[target_feature]
+
+    # Drop respondent and expected salary columns
+    df = df.drop(['Respondent', , target_feature], axis=1)
+
+    # Fill numeric columns with the mean
+    num_vars = df.select_dtypes(include=['float', 'int']).columns
+    for col in num_vars:
+        df[col].fillna((df[col].mean()), inplace=True)
+
+    # Dummy the categorical variables
+    cat_vars = df.select_dtypes(include=['object']).copy().columns
+    for var in cat_vars:
+        # for each cat add dummy var, drop original column
+        df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)],
+                       axis=1)
+
+    X = df
+    return X, y
---
+++ preparation/data_load.py
@@ -0,0 +1,84 @@
+"""
+This file contains functions needed to load data from sources
+"""
+import pandas as pd
+import os
+
+
+def load_from_csv(file_path: str, encoding: str):
+    """
+    Loads single year survey data from CSV file
+    :param year: reference year
+    :param file_path: file path to get data from
+    :param encoding: cvs source file encoding
+    :return: a dataframe containing raw data from survey from a single year
+    """
+
+    # in case no path is provided, assuming default path
+    return pd.read_csv(file_path, encoding=encoding)
+
+
+def load_surveys_data_from_csv(years=None, data_path="data", encoding="ISO-8859-1"):
+    """
+    Loads multiple years survey data from CSV files
+    :param years: a list of multiple years in integer format
+    :param data_path: data folder where CSV files is expected to be located
+    :param encoding: csv files encoding
+    :return: a dictionary of dataframes containing raw data from surveys from multiple years
+    """
+    if years is None:
+        years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+
+    # dictionary containing years dat
+    surveys_years_df = {}
+    # retrieving base directory where data folder is expected to be located
+    base_dir = os.path.split(os.getcwd())[0]
+    for y in years:
+        surveys_years_df[y] = load_from_csv(os.path.join(base_dir, data_path,
+                                                         '{}_results.csv'.format(y)), encoding)
+    return surveys_years_df
+
+
+def get_intersection(features_list1: list, features_list2: list) -> list:
+    """
+    Get intersection between two list of features as strings list
+    :param features_list1: first list of features
+    :param features_list2: second list of features
+    :return: a list of string, composed of all elements both in features_list1 and features_list2
+    """
+    features_intersection = [value for value in features_list1 if value in features_list2]
+    return features_intersection
+
+
+def get_common_feature_list(data_frames_dict: dict) -> list:
+    """
+    Computes least common features set from dataframes dictionary
+    :param data_frames_dict: dataframe dictionary in the form of {year : dataframe}
+    :return: least common features set in a dict of dataframes
+    """
+
+    # computing common features
+    features = []
+    for i, df in enumerate(data_frames_dict.values()):
+        if i == 0:
+            features = df.columns
+        else:
+            features = get_intersection(features, df.columns)
+    return features
+
+
+def merge_dataframes(data_frames_dict):
+    """
+    Merges dataframes based on least common feature set
+    :param data_frames_dict: a dataframes dictionary data to be merged, based on least common features
+    :return: a single, merged dataframe based on least common feature set
+    """
+    merged_df = None
+    common_features_list = get_common_feature_list(data_frames_dict)
+    for i, (year, df) in enumerate(data_frames_dict.items()):
+        df['year'] = year
+        if i == 0:
+            merged_df = df
+        else:
+            merged_df = pd.concat([merged_df[common_features_list], df[common_features_list]])
+    return merged_df
