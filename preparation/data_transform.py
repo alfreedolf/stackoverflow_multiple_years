b7b1fe2b9bc72fd055d34035fbc50b2d7cd6864c
Alfonso Ridolfo <alfonso.ridolfo@gmail.com>
2021-11-21 01:15:40 +0100

    feat: first complete languages trend analysis
    Languages popularity dataframe created.
    Plot of languages trend done.

--- preparation/data_transform.py
+++ preparation/data_transform.py
@@ -211,41 +211,12 @@ def feature_split(df: pd.DataFrame,
 
 
 def string_found(string1, string2):
+    """
+    This function looks for a string
+    :param string1:
+    :param string2:
+    :return:
+    """
     if re.search(r"\b" + re.escape(string1) + r"\b", string2):
         return True
     return False
-
-# TODO: check it and think about deleting it
-# def clean_data(df, target_feature):
-#     """
-#     This function cleans df using the following steps to produce X and y:
-#     1. Drop all the rows with no preferred language
-#     2. Create X as all the columns that are not the Salary column
-#     3. Create y as the Salary column
-#     4. For each numeric variable, fill the column with the mean value.
-#     6. Create dummy columns for all the categorical variables, drop the original columns
-#     :param df dataframe to be cleaned
-#     :returns X - A matrix holding all of the variables you want to consider when predicting the response
-#              y - the corresponding response vector
-#     """
-#     # Drop rows with missing target_feature values
-#     df = df.dropna(subset=[target_feature], axis=0)
-#     y = df[target_feature]
-#
-#     # Drop respondent and expected salary columns
-#     df = df.drop(target_feature, axis=1)
-#
-#     # Fill numeric columns with the mean
-#     num_vars = df.select_dtypes(include=['float', 'int']).columns
-#     for col in num_vars:
-#         df[col].fillna((df[col].mean()), inplace=True)
-#
-#     # Dummy the categorical variables
-#     cat_vars = df.select_dtypes(include=['object']).copy().columns
-#     for var in cat_vars:
-#         # for each cat add dummy var, drop original column
-#         df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)],
-#                        axis=1)
-#
-#     X = df
-#     return X, y
