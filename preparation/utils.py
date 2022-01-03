1d4e933e707a1a6409bc756e95c43505891b7365
Your-Full-Name <alfonso.ridolfo@gmail.com>
2022-01-03 19:06:51 +0100

    Chore: refactor color palette mapping code

---
+++ preparation/utils.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def map_languages_to_color_list(languages_color_palette: dict, s_2011_proficiencies_top_10: pd.Series):
+    # mapping values from dictionary to palette color list to be fed to plot bar code
+    return [languages_color_palette[s_2011_proficiencies_top_10.index[i]] for i in
+            range(len(s_2011_proficiencies_top_10))]
