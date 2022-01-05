0a3cfc638f0e5e3ae016b2bdada9d2758e51567c
alfreedolf <alfonso.ridolfo@gmail.com>
2022-01-05 20:32:56 +0100

    Chore: initialize LanguagesStatsExtractor class' test cases

---
+++ test/test_utils.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestLanguagesStatsExtractor(TestCase):
+    def test_compute_top_ten_languages(self):
+        self.fail()
+
+
+class TestLanguagesStatsExtractor(TestCase):
+    def test_compute_language_proficiency_ranking(self):
+        self.fail()
