1a8712ac04666b363660a412c80ad5ac2461b2ba
alfreedolf <alfonso.ridolfo@gmail.com>
2022-03-05 22:48:18 +0100

    Chore: fix to typo in prefix name

--- test/test_data_stats.py
+++ test/test_data_stats.py
@@ -19,25 +19,35 @@ from preparation.data_stats import map_any_case_to_lower, drop_columns_from_map,
 class TestDropColumnsFromLowerCaseMap(TestCase):
     def setUp(self) -> None:
         # input output vectors
-        self.any_case_input = ['Proficient in Java', 'Proficient in JavaScript', 'Proficient in Javascript']
+        self.any_case_input = ['Proficient in Java', 'Proficient in JavaScript', 'Proficient in Javascript',
+                               "Proficient in Node.js"]
         self.df_input = pd.DataFrame(index=[1, 2, 3, 4, 5], columns=self.any_case_input)
         self.column_to_drop = 'Proficient in JavaScript'
 
         # expected output vectors
         self.expected_output_map = {'proficient in java': ['Proficient in Java'],
                                     'proficient in javascript': ['Proficient in JavaScript',
-                                                                 'Proficient in Javascript']}
-        self.expected_output_columns = ['Proficient in Java']
+                                                                 'Proficient in Javascript'],
+                                    'proficient in node.js': ['Proficient in Node.js']
+                                    }
+        self.expected_output_columns = ['Proficient in Java', 'Proficient in Node.js']
         self.df_expected_output = pd.DataFrame(index=[1, 2, 3, 4, 5], columns=self.expected_output_columns)
 
     def test_map_any_case_to_lower(self):
         out_map = map_any_case_to_lower(self.any_case_input)
-        self.assertEqual(out_map, self.expected_output_map)
+        inequalities = 0
+        for key in self.expected_output_map:
+            val1 = self.expected_output_map[key]
+            val2 = out_map[key]
+            if key not in out_map or (val1 != val2):
+                inequalities += 1
+
+        self.assertFalse(inequalities > 0)
 
     def test_drop_columns_from_map(self):
         df_output = drop_columns_from_map(self.df_input, dropping_map=self.expected_output_map,
                                           column_to_drop=self.column_to_drop.lower())
-        self.assertEqual(df_output.columns, self.df_expected_output.columns)
+        np.testing.assert_array_equal(df_output.columns, self.df_expected_output.columns)
 
 
 class TestLanguagesRankingExtractor(TestCase):
@@ -67,15 +77,21 @@ class TestLanguagesRankingExtractor(TestCase):
                                         'Objective-C']
         self.s_expected_output = pd.Series(index=self.s_expected_output_index, data=[4, 3, 3, 3, 2, 2, 2, 2, 1, 1])
 
+        self.expected_output_columns_after_merge = ['Proficient in Java', 'Proficient in C++',
+                                                    'Proficient in C#', 'Proficient in Python',
+                                                    'Proficient in Ruby', 'Proficient in C', 'Proficient in JavaScript',
+                                                    'Proficient in Objective-C', 'Proficient in Visual Basic',
+                                                    'Proficient in PHP']
+
         # entries merge list
-        self.entries_merge_list = [""]
+        self.entries_merge_list = [('JavaScript', 'Node.js'), ('JavaScript', 'jQuery')]
 
         # input LanguagesStatsExtractor
         self.lre = LanguagesRankingExtractor(source_data=self.df_input, prefix_to_remove='Proficient in ',
-                                             entries_merge_list=[('JavaScript', 'Node.js'), ('JavaScript', 'jQuery')])
+                                             entries_merge_list=self.entries_merge_list)
 
     def test_compute_top_ten_languages(self):
-        top_ten_languages_df: pd.DataFrame = self.lre.compute_top_ten_languages()
+        top_ten_languages_df: pd.Series = self.lre.compute_top_ten_languages()
         # check for index equality
         with self.subTest():
             np.testing.assert_array_equal(top_ten_languages_df.index.values,
@@ -94,6 +110,7 @@ class TestLanguagesRankingExtractor(TestCase):
         # TODO add test
         pass
 
-    def test_merge_entries(selfs):
+    def test_merge_entries(self):
         # TODO add test
-        pass
+        self.lre.merge_entries(df_proficiencies=self.df_input, entries_merge_list=self.entries_merge_list)
+        np.testing.assert_array_equal(self.df_input.columns, self.expected_output_columns_after_merge)
