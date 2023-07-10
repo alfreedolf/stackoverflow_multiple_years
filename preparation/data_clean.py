c8aa323ab652921d107a72e2e366f76ae9a97c3f
Alfonso Ridolfo <alfonso.ridolfo@gmail.com>
2023-07-10 18:16:20 +0200

    chore: apply pep8 to data_clean script

--- preparation/data_clean.py
+++ preparation/data_clean.py
@@ -1,48 +1,69 @@
+"""
+Implement function for data cleaning purposes.
+
+Author: Alfonso Ridolfo
+Date: October 2021
+"""
+
+
 import pandas as pd
 
 
-def clean_data(df, target_feature):
-    """
-    This function cleans df using the following steps to produce X and y:
+def clean_data(input_dataframe, target_feature: str):
+    """Clean a dataframe with respect to a target feature.
+
+    Use the following steps to produce X (features) and y (labels) as output:
+
     1. Drop all the rows with no preferred language
     2. Create X as all the columns that are not the Salary column
     3. Create y as the Salary column
-    4. For each numeric variable, fill the column with the mean value.
-    6. Create dummy columns for all the categorical variables, drop the original columns
-    :param df dataframe to be cleaned
-    :param target_feature: target feature to be cleaned
-    :returns X - A matrix holding all the variables you want to consider when predicting the response
-             y - the corresponding response vector
+    4. For each numeric variable, fill the column with the mean value
+    6. Create dummy columns for all the categorical variables
+    7. Drop original columns.
+
+    Args:
+        input_dataframe: (pandas.DataFrame) dataframe to be cleaned
+        target_feature: (str) target feature to be cleaned
+
+    Returns:
+        input_features: (pandas.DataFrame) matrix containing meaninful variables
+        target_label: (pandas.DataFrame) corresponding answer vector
     """
     # Drop rows with missing target_feature values
-    df = df.dropna(subset=[target_feature], axis=0)
-    y = df[target_feature]
+    input_dataframe = input_dataframe.dropna(subset=[target_feature], axis=0)
+    target_label = input_dataframe[target_feature]
 
     # Drop respondent and expected salary columns
     # TODO fix
-    # df = df.drop(['Respondent', , target_feature], axis=1)
+    # input_dataframe = input_dataframe.drop(['Respondent', , target_feature],
+    # axis=1)
 
     # Fill numeric columns with the mean
-    num_vars = df.select_dtypes(include=['float', 'int']).columns
+    num_vars = input_dataframe.select_dtypes(include=['float', 'int']).columns
     for col in num_vars:
-        df[col].fillna((df[col].mean()), inplace=True)
+        input_dataframe[col].fillna(
+            (input_dataframe[col].mean()), inplace=True)
 
     # Dummy the categorical variables
-    cat_vars = df.select_dtypes(include=['object']).copy().columns
+    cat_vars = input_dataframe.select_dtypes(include=['object']).copy().columns
     for var in cat_vars:
         # for each cat add dummy var, drop original column
-        df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)],
-                       axis=1)
+        input_dataframe = pd.concat([input_dataframe.drop(var, axis=1), pd.get_dummies(
+            input_dataframe[var], prefix=var, prefix_sep='_', drop_first=True)], axis=1)
 
-    X = df
-    return X, y
+    input_features = input_dataframe
+    return input_features, target_label
+
 
 def calculate_time_between_dates(date1, date2):
-    """
-    This function calculates the time between two dates
-    :param date1: first date
-    :param date2: second date
-    :returns time_between_dates: time between two dates
+    """Calculate time elapsed between two input dates.
+
+    Args:
+        date1: first date
+        date2: second date
+
+    Returns:
+        time_between_dates: time between two dates
     """
     time_between_dates = abs(date1 - date2)
     return time_between_dates.days
