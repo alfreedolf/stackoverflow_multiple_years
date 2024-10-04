2e74e0bbd482fcd87da88bfd7e1abe6e2a31bfe6
alfreedolf <alfonso.ridolfo@gmail.com>
2024-10-05 01:27:25 +0200

    feat: small improvements to plots and notebooks

--- preparation/utils.py
+++ preparation/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def map_languages_to_color_list(languages_color_palette: dict, s_2011_proficiencies_top_10: pd.Series) -> list:
+def map_languages_to_color_list(languages_color_palette: dict, proficiencies_top_10: pd.Series) -> list:
     # mapping values from dictionary to palette color list to be fed to plot bar code
-    return [languages_color_palette[s_2011_proficiencies_top_10.index[i]] for i in
-            range(len(s_2011_proficiencies_top_10))]
+    return [languages_color_palette[proficiencies_top_10.index[i]] for i in
+            range(len(proficiencies_top_10))]
